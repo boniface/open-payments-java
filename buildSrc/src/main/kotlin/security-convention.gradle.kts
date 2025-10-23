3a9c852c673bb700682396dcd7ed71038e392a3b
boniface <550236+boniface@users.noreply.github.com>
2025-10-22 20:12:04 -0400

    fix: updated Security checks for env

--- buildSrc/src/main/kotlin/security-convention.gradle.kts
+++ buildSrc/src/main/kotlin/security-convention.gradle.kts
@@ -9,4 +9,7 @@ dependencyCheck {
     suppressionFile = "${project.rootDir}/config/dependency-check/suppressions.xml"
     failBuildOnCVSS = 7.0f
     analyzers.assemblyEnabled = false
+
+    // Use NVD API Key from environment variable if available
+    nvd.apiKey = System.getenv("NVD_API_KEY") ?: ""
 }
